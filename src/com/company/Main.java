05401ff4eadf6a63e3fdace7fa90a7d53b1c68dd
Padr02 <pavel.drepalov@gmail.com>
2021-12-27 11:35:13 +0100

    tal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Pavel");
     Scanner scanner = new Scanner(System.in);
+    int tal = scanner.nextInt();
+    System.out.println(tal);
     }
 }
